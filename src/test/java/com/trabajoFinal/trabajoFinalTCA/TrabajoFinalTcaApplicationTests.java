10e16fd9ece359e1ad17fce94eb1bfe2ea074697
LencinaJuanFrancisco <81085476+LencinaJuanFrancisco@users.noreply.github.com>
2023-10-18 16:23:41 -0300

    spring initializer

---
+++ src/test/java/com/trabajoFinal/trabajoFinalTCA/TrabajoFinalTcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajoFinal.trabajoFinalTCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalTcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
